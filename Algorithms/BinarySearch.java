54c389e86be440b5ada5c2ebc8b92923b14b3cff
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2016-05-10 14:57:18 +0200

    BinarySearch kommentiert

--- Algorithms/BinarySearch.java
+++ Algorithms/BinarySearch.java
@@ -1,6 +1,8 @@
 /* Binary Search
  ** \log n
- * Binary searchs for an element in a sorted array.
+ * Binary searchs for an element in a sorted array. \\
+ * \emph{Input:} sorted $array$ to search in, amount $N$ of elements in $array$, element to search for $a$ \\
+ * \emph{Output:} \texttt{true}, if $array$ contains $a$, \texttt{false} otherwise
  */
 
 class Main {
@@ -9,22 +11,27 @@ class Main {
   }
 
 //START
-public static boolean BinarySearch(int[] array, int N, int a) {
+public static boolean BinarySearch(int[] array,
+                                   int N, int a) {
 	int lo = 0;
 	int hi = N-1;
+	// a might be in interval [lo,hi] while lo <= hi
 	while(lo <= hi) {
 		int mid = (int) (((lo + hi) / 2.0) + 0.6);
-		if(array[mid] < a) {
+		// if a > elem in mid of interval,
+		// search the right subinterval
+		if(array[mid] < a)
 			lo = mid+1;
-		} else {
+		// else search the left subinterval
+		else
 			hi = mid-1;
-		}
 	}
-	if(lo < N  && array[lo] == a) {
+	// lo < N avoids ArrayOutOfBoundsException
+	// if array[lo] == a, array contains a
+	if(lo < N  && array[lo] == a)
 		return true;
-	} else {
+	else
 		return false;
-	}
 }
 //END
 
