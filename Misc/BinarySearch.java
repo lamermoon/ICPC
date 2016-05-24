5fb4fea98fb455771d44299edae652c159c0fcf9
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2016-05-24 14:49:01 +0200

    BinarySearch verkürzt

--- Misc/BinarySearch.java
+++ Misc/BinarySearch.java
@@ -2,36 +2,41 @@
  ** \log n
  * Binary searchs for an element in a sorted array. \\
  * \emph{Input:} sorted $array$ to search in, amount $N$ of elements in $array$, element to search for $a$ \\
- * \emph{Output:} \texttt{true}, if $array$ contains $a$, \texttt{false} otherwise
+ * \emph{Output:} returns the index of $a$ in $array$ or $-1$ if $array$ does not contain $a$
  */
 
 class Main {
   public static void main(String[] args) {
-	  
+		int a = Integer.parseInt(args[0]);
+		int[] arr = new int[args.length-1];
+		for(int i = 1; i < args.length; i++){
+			arr[i-1] = Integer.parseInt(args[i]);
+		}
+		System.out.println(BinarySearch(arr, arr.length, a));
   }
 
 //START
-public static boolean BinarySearch(int[] array,
+public static int BinarySearch(int[] array,
                                    int N, int a) {
 	int lo = 0;
 	int hi = N-1;
 	// a might be in interval [lo,hi] while lo <= hi
 	while(lo <= hi) {
-		int mid = (int) (((lo + hi) / 2.0) + 0.6);
+		int mid = (lo + hi) / 2;
 		// if a > elem in mid of interval,
 		// search the right subinterval
 		if(array[mid] < a)
 			lo = mid+1;
-		// else search the left subinterval
-		else
+		// else if a < elem in mid of interval,
+		// search the left subinterval
+		else if(array[mid] > a)
 			hi = mid-1;
+		// else a is found
+		else
+			return mid;
 	}
-	// lo < N avoids ArrayOutOfBoundsException
-	// if array[lo] == a, array contains a
-	if(lo < N  && array[lo] == a)
-		return true;
-	else
-		return false;
+	// array does not contain a
+	return -1;
 }
 //END
 
