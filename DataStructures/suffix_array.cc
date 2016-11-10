464e8958d94c1b130d16c2490aa652d97b446c5d
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2016-11-10 20:26:19 +0100

    Einrückung Suffix-Array korrigiert

--- DataStructures/suffix_array.cc
+++ DataStructures/suffix_array.cc
@@ -14,69 +14,69 @@ string s;
 int N, gap;
 
 bool sufCmp(int i, int j) {
-    if(pos[i] != pos[j])
-	return pos[i] < pos[j];
-    i += gap;
-    j += gap;
-    return (i < N && j < N) ? pos[i] < pos[j] : i > j;
+  if(pos[i] != pos[j])
+    return pos[i] < pos[j];
+  i += gap;
+  j += gap;
+  return (i < N && j < N) ? pos[i] < pos[j] : i > j;
 }
 
 void buildSA()
 {
-    N = s.size();
+  N = s.size();
+  for(int i = 0; i < N; ++i) {
+    sa.push_back(i);
+    pos.push_back(s[i]);
+  }
+  tmp.resize(N);
+  for(gap = 1;;gap *= 2) {
+    sort(sa.begin(), sa.end(), sufCmp);
+    for(int i = 0; i < N - 1; ++i) {
+      tmp[i+1] = tmp[i] + sufCmp(sa[i], sa[i+1]);
+    }
     for(int i = 0; i < N; ++i) {
-	sa.push_back(i);
-	pos.push_back(s[i]);
+      pos[sa[i]] = tmp[i];
     }
-    tmp.resize(N);
-    for(gap = 1;;gap *= 2) {
-	sort(sa.begin(), sa.end(), sufCmp);
-	for(int i = 0; i < N - 1; ++i) {
-	    tmp[i+1] = tmp[i] + sufCmp(sa[i], sa[i+1]);
-	}
-	for(int i = 0; i < N; ++i) {
-	    pos[sa[i]] = tmp[i];
-	}
-	if(tmp[N-1] == N-1) break;
-    }
+    if(tmp[N-1] == N-1) break;
+  }
 }
 
 void buildLCP()
 {
-    lcp.resize(N);
-    for(int i = 0, k = 0; i < N; ++i) {
-	if(pos[i] != N - 1) {
-	    for(int j = sa[pos[i] + 1]; s[i + k] == s[j + k];) {
-		++k;
-	    }
-	    lcp[pos[i]] = k;
-	    if (k) --k;
-	}
+  lcp.resize(N);
+  for(int i = 0, k = 0; i < N; ++i) {
+    if(pos[i] != N - 1) {
+      for(int j = sa[pos[i] + 1]; s[i + k] == s[j + k];) {
+        ++k;
+      }
+      lcp[pos[i]] = k;
+      if (k) --k;
     }
+  }
 }
 
 int main()
 {
-    string r, t;
-    cin >> r >> t;
-    s = r + "$" + t;
-    buildSA();
-    buildLCP();
-    for(int i = 0; i < N; ++i) {
-	cout << sa[i] << " " << lcp[i] << endl;
+  string r, t;
+  cin >> r >> t;
+  s = r + "§" + t;
+  buildSA();
+  buildLCP();
+  for(int i = 0; i < N; ++i) {
+    cout << sa[i] << " " << lcp[i] << endl;
+  }
+  int mx = 0, mxi = -1;
+  for(int i = 0; i+1 < s.size(); ++i) {
+    bool a_in_s = sa[i] < r.size(), b_in_s = sa[i+1] < r.size();
+    if(a_in_s != b_in_s) {
+      int l = lcp[i];
+      if(l > mx) {
+        mx = l;
+        mxi = sa[i];
+      }
     }
-    int mx = 0, mxi = -1;
-    for(int i = 0; i+1 < s.size(); ++i) {
-	bool a_in_s = sa[i] < r.size(), b_in_s = sa[i+1] < r.size();
-	if(a_in_s != b_in_s) {
-	    int l = lcp[i];
-	    if(l > mx) {
-		mx = l;
-		mxi = sa[i];
-	    }
-	}
-    }
-    cout << mx << endl;
-    cout << s.substr(mxi, mx) << endl;
+  }
+  cout << mx << endl;
+  cout << s.substr(mxi, mx) << endl;
 }
 //END
