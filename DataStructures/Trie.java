d1347e5389ad8352f4b0db50a49523ccc81b4425
Thore Tiemann <thore.tiemann@informatik.uni-luebeck.de>
2016-11-13 17:48:02 +0100

    Trie Einrückung korrigiert und kompiliert

--- DataStructures/Trie.java
+++ DataStructures/Trie.java
@@ -4,48 +4,48 @@
 
 //START
 public static boolean insert(TrieNode root, String word){
-    char[] s = word.toCharArray();
-    TrieNode node = root;
-    
-    for(int i = 0; i < s.length; ++i){
-        int index = charToIndex(s[i]);
-        if(node.children[index] == null){
-            node.children[index] = new TrieNode(node);
-        }
-        node = node.children[index];
+  char[] s = word.toCharArray();
+  TrieNode node = root;
+
+  for(int i = 0; i < s.length; ++i){
+    int index = charToIndex(s[i]);
+    if(node.children[index] == null){
+      node.children[index] = new TrieNode(node);
     }
-    node.isEnd = true;
-    
-    return true;
+    node = node.children[index];
+  }
+  node.isEnd = true;
+
+  return true;
 }
 
 public static boolean search(TrieNode root, String word){
-    char[] s = word.toCharArray();
-    TrieNode node = root;
-    
-    for(int i = 0; i < s.length; ++i){
-        int index = charToIndex(s[i]);
-        if(node.children[index] == null){
-            return false;
-        }
-        node = node.children[index];
+  char[] s = word.toCharArray();
+  TrieNode node = root;
+
+  for(int i = 0; i < s.length; ++i){
+    int index = charToIndex(s[i]);
+    if(node.children[index] == null){
+      return false;
     }
-    
-    return node.isEnd;
+    node = node.children[index];
+  }
+
+  return node.isEnd;
 }
 
 public static int charToIndex(char c){
-    return ((int) c - (int) 'a');
+  return ((int) c - (int) 'a');
 }
 
 static class TrieNode{
-    
-    boolean isEnd;
-    TrieNode[] children;
-    
-    public TrieNode(){
-        isEnd = false;
-        children = new TrieNode[26];
-    }
+
+  boolean isEnd;
+  TrieNode[] children;
+
+  public TrieNode(){
+    isEnd = false;
+    children = new TrieNode[26];
+  }
 }
 //END
